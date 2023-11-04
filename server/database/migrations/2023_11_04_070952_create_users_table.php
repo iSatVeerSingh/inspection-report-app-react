3bb7c56915f7a02ce6bda8094ffcf4e8a18db4d8
iSatVeerSingh <developer.satveer@gmail.com>
2023-11-04 13:03:45 +0530

    users migration created

--- server/database/migrations/2023_11_04_070952_create_users_table.php
+++ server/database/migrations/2023_11_04_070952_create_users_table.php
@@ -13,11 +13,13 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->uuid('uuid')->unique();
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('phone', 12)->unique();
+            $table->string('avatar');
             $table->string('password');
-            $table->rememberToken();
+            $table->enum('role', ['owner', 'admin', 'inspector']);
             $table->timestamps();
         });
     }
