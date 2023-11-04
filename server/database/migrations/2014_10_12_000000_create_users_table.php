1aca8d4b044ab25c7fe9b5209ffa92e02b25eb2a
iSatVeerSingh <developer.satveer@gmail.com>
2023-11-04 16:34:47 +0530

    laravel setup

--- server/database/migrations/2014_10_12_000000_create_users_table.php
+++ server/database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,13 +13,11 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->uuid('uuid')->unique();
             $table->string('name');
             $table->string('email')->unique();
-            $table->string('phone', 12)->unique();
-            $table->string('avatar');
+            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->enum('role', ['owner', 'admin', 'inspector']);
+            $table->rememberToken();
             $table->timestamps();
         });
     }
